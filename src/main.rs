164de6fe91994a84b3348af3974cc057c0837a29
charSLee <735365895@qq.com>
2023-01-29 21:57:25 +0800

    only test for remote machine

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use rand::seq::index::sample;
 use std::collections::HashSet;
 use std::net::IpAddr;
+use url::quirks::host;
 
+use async_std::io::ReadExt;
+use download::Downloader;
 use futures::executor::block_on;
 use input::Opts;
 
@@ -31,9 +35,53 @@ fn main() {
         Duration::from_millis(opts.timeout),
         opts.time,
         opts.port,
+        opts.avg_delay_upper,
+        opts.avg_delay_lower,
     );
     let mut result = block_on(scanner.run());
     result.sort();
+
+    // display result
+    for r in result.iter().take(opts.display) {
+        println!("{}", r);
+    }
+
+    if opts.disable_download.is_some() {
+        println!("Disable download speed test.exiting...");
+        return;
+    }
+
+    let download_ips: Vec<std::net::IpAddr> = result
+        .iter()
+        .take(opts.download_number)
+        .map(|x| x.ip)
+        .collect();
+
+    let domain: String = match utils::get_domain_from_url(opts.download_url.as_str()) {
+        Ok(h) => h,
+        Err(e) => {
+            println!("Cannot get host for speed test url;\nError message: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    // download speed test
+    let downloader: Downloader = Downloader::new(
+        &download_ips,
+        4,
+        domain,
+        Duration::from_secs(10),
+        Duration::from_millis(5000),
+        opts.download_port,
+        opts.download_url.to_string(),
+    );
+
+    let mut speedtest_result = downloader.run();
+    speedtest_result.sort();
+
+    for r in speedtest_result.iter() {
+        println!("{}", r);
+    }
 
     // write result to file
     match utils::write_to_csv(&opts.output, &result) {
@@ -45,24 +93,27 @@ fn main() {
             );
         }
     }
-
-    // display result
-    for r in result.iter().take(opts.display) {
-        println!("{}", r);
-    }
 }
 
 fn parse_addresses_from_opt(opts: &Opts) -> Vec<IpAddr> {
     let mut ips: Vec<IpAddr> = Vec::new();
     for arg in opts.args.iter() {
-        match std::fs::read_to_string(arg) {
-            Ok(content) => {
-                ips.extend(utils::parse_addresses(&content));
-            }
-            Err(_) => {
-                ips.extend(utils::parse_addresses(arg));
-            }
-        }
+        let content: String = match std::fs::read_to_string(arg) {
+            Ok(text) => text,
+            Err(_) => arg.to_string(),
+        };
+        let parse_ips = utils::parse_addresses(&content);
+        let sample_size = if opts.random_number > 0 && opts.random_number < parse_ips.len() {
+            opts.random_number
+        } else {
+            parse_ips.len()
+        };
+
+        let parse_ips: Vec<IpAddr> = sample(&mut rand::thread_rng(), parse_ips.len(), sample_size)
+            .into_iter()
+            .map(|i| parse_ips[i])
+            .collect();
+        ips.extend(parse_ips.iter());
     }
 
     let set: HashSet<IpAddr> = ips.into_iter().collect();
@@ -72,43 +123,140 @@ fn parse_addresses_from_opt(opts: &Opts) -> Vec<IpAddr> {
 #[cfg(test)]
 mod test {
     use std::net::IpAddr;
+    use std::str::FromStr;
+    use std::time::Duration;
+
+    use crate::download::Downloader;
 
     use super::scanner;
 
     use super::utils;
     use async_std::task::block_on;
     use rand::seq::SliceRandom;
+    use tokio::runtime::Runtime;
+
+    fn default_test_ips() -> String {
+        return "173.245.48.0/20
+        103.21.244.0/22
+        103.22.200.0/22
+        103.31.4.0/22
+        141.101.64.0/18
+        108.162.192.0/18
+        190.93.240.0/20
+        188.114.96.0/20
+        197.234.240.0/22
+        198.41.128.0/17
+        162.158.0.0/15
+        104.16.0.0/13
+        104.24.0.0/14
+        172.64.0.0/13
+        131.0.72.0/22"
+            .to_string();
+    }
 
     #[test]
-    // / Makes sure the network is available
-    pub fn speedtest_from_cloudflare() {
-        let ips_v4 = "173.245.48.0/20
-103.21.244.0/22
-103.22.200.0/22
-103.31.4.0/22
-141.101.64.0/18
-108.162.192.0/18
-190.93.240.0/20
-188.114.96.0/20
-197.234.240.0/22
-198.41.128.0/17
-162.158.0.0/15
-104.16.0.0/13
-104.24.0.0/14
-172.64.0.0/13
-131.0.72.0/22";
-        let ips = utils::parse_addresses(ips_v4);
+    #[ignore]
+    pub fn tcpdelay_from_cloudflare() {
+        let ips_v4 = default_test_ips();
+        let ips = utils::parse_addresses(&ips_v4);
         assert!(!ips.is_empty());
 
-        // 随机测试5000个IP以免耗费过长时间
+        // 随机测试4096个IP以免耗费过长时间
         let scan_ips: Vec<IpAddr> = ips
-            .choose_multiple(&mut rand::thread_rng(), 5958)
+            .choose_multiple(&mut rand::thread_rng(), 4096)
             .cloned()
             .collect();
-        let scan =
-            scanner::Scanner::new(scan_ips, 500, std::time::Duration::from_millis(5000), 4, 80);
+        let scan = scanner::Scanner::new(
+            scan_ips,
+            500,
+            std::time::Duration::from_millis(5000),
+            4,
+            443,
+            9999,
+            0,
+        );
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let mut result = rt.block_on(scan.run());
+        result.sort();
+        assert!(!result.is_empty());
+
+        // show top 10
+        for r in result.iter().take(10) {
+            println!("{}", r);
+        }
+    }
+
+    #[ignore]
+    #[test]
+    // / Makes sure the network is available
+    pub fn fulltest_from_cloudflare() {
+        let ips_v4 = &default_test_ips();
+        let ips = utils::parse_addresses(&ips_v4);
+        assert!(!ips.is_empty());
+
+        // 随机测试1024个IP以免耗费过长时间
+        let scan_ips: Vec<IpAddr> = ips
+            .choose_multiple(&mut rand::thread_rng(), 1024)
+            .cloned()
+            .collect();
+        let scan = scanner::Scanner::new(
+            scan_ips,
+            200,
+            std::time::Duration::from_millis(5000),
+            4,
+            443,
+            9999,
+            0,
+        );
         let mut result = block_on(scan.run());
         result.sort();
         assert!(!result.is_empty());
+
+        let mut speed_test_ips = Vec::new();
+        for r in result.iter().take(15) {
+            println!("{}", r);
+            speed_test_ips.push(r.ip);
+        }
+
+        // download speed test
+        let downloader: Downloader = Downloader::new(
+            &speed_test_ips,
+            4,
+            "speed.cloudflare.com".to_string(),
+            Duration::from_secs(10),
+            Duration::from_millis(5000),
+            80,
+            "http://speed.cloudflare.com/__down?bytes=2000000000".to_string(),
+        );
+
+        for r in downloader.run().iter() {
+            println!("{}", r);
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn downloadtest_from_cloudflare() {
+        let speed_test_ips = vec![
+            IpAddr::from_str("173.245.58.109").unwrap(),
+            IpAddr::from_str("173.245.59.176").unwrap(),
+            IpAddr::from_str("173.245.59.148").unwrap(),
+            IpAddr::from_str("173.245.59.176").unwrap(),
+        ];
+
+        // download speed test
+        let downloader: Downloader = Downloader::new(
+            &speed_test_ips,
+            4,
+            "speed.cloudflare.com".to_string(),
+            Duration::from_secs(30),
+            Duration::from_millis(5000),
+            443,
+            "https://speed.cloudflare.com/__down?bytes=2000000000".to_string(),
+        );
+
+        for r in downloader.run().iter() {
+            println!("{}", r);
+        }
     }
 }
--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,15 +1,17 @@
 use std::{
     cmp::Ordering,
     fmt,
+    net::{IpAddr, SocketAddr},
     num::NonZeroU8,
     time::{Duration, Instant},
 };
 
-use async_std::io;
-use async_std::net::{IpAddr, Shutdown, SocketAddr, TcpStream};
-use async_std::prelude::*;
-use futures::stream::FuturesUnordered;
+// use async_std::io;
+// use async_std::net::{IpAddr, Shutdown, SocketAddr, TcpStream};
+// use async_std::prelude::*;
+// use futures::{select, stream::FuturesUnordered, StreamExt};
 use indicatif::{ProgressBar, ProgressStyle};
+use tokio::{io::AsyncWriteExt, net::tcp, sync::mpsc, time::Timeout};
 
 #[derive(Debug)]
 // 扫描基本设置
@@ -17,17 +19,29 @@ pub struct Scanner {
     // 测试IP地址集合
     ips: Vec<IpAddr>,
     // 同时测试的最大数量
-    batch_size: u16,
+    batch_size: usize,
     // 同个IP测试的次数
     times: NonZeroU8,
     // 超时设置
     timeout: Duration,
     // 设定端口
     port: u16,
+    // 平均延迟上限
+    avg_delay_upper: u128,
+    // 平均延迟下限
+    avg_delay_lower: u128,
 }
 
 impl Scanner {
-    pub fn new(ips: Vec<IpAddr>, batch_size: u16, timeout: Duration, times: u8, port: u16) -> Self {
+    pub fn new(
+        ips: Vec<IpAddr>,
+        batch_size: usize,
+        timeout: Duration,
+        times: u8,
+        port: u16,
+        avg_delay_upper: u128,
+        avg_delay_lower: u128,
+    ) -> Self {
         let batch_size = if batch_size == 0 { 1 } else { batch_size };
 
         let times = if times == 0 { 1 } else { times };
@@ -46,24 +60,16 @@ impl Scanner {
             timeout,
             times: NonZeroU8::new(times).unwrap_or(NonZeroU8::new(1).unwrap()),
             port,
+            avg_delay_upper,
+            avg_delay_lower,
         }
     }
 
     pub async fn run(&self) -> Vec<Delay> {
-        // 创建socketAddr的迭代器
-        let mut socket_addrs = self.ips.iter().map(|ip| SocketAddr::new(*ip, self.port));
+        // create a channel for sending tasks to the pool
+        let (tx, mut rx) = mpsc::channel(self.batch_size);
 
-        let mut res: Vec<Delay> = Vec::new();
-        let mut ftrs = FuturesUnordered::new();
-
-        // 这里创建连接池的大小为 batch_size
-        for _ in 0..self.batch_size {
-            if let Some(socket) = socket_addrs.next() {
-                ftrs.push(self.tcp_socket(socket));
-            }
-        }
-
-        // 进度条
+        let mut res = Vec::new();
         let total = self.ips.len();
         let pb = ProgressBar::new(total as u64);
         pb.set_style(
@@ -74,42 +80,113 @@ impl Scanner {
             .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ "),
         );
 
-        // 只有连接池有空余才塞入任务
-        while let Some(result) = ftrs.next().await {
-            if let Some(socket) = socket_addrs.next() {
-                ftrs.push(self.tcp_socket(socket));
-            }
+        let ips = self.ips.clone();
+        for ip in &ips {
+            let tx = tx.clone();
+            let socket = SocketAddr::new(*ip, self.port);
+            let times = self.times;
+            let timeout = self.timeout;
 
-            match result {
-                Ok(delay) => {
-                    pb.set_message(format!("Addr: {}", delay.ip));
-                    pb.inc(1);
-
-                    res.push(delay);
-                }
-                Err(_) => {
-                    pb.inc(1);
-                }
-            }
+            tokio::spawn(async move {
+                let delay = Scanner::tcp_socket(times, timeout, socket).await;
+                tx.send(delay).await.unwrap();
+            });
         }
+        for _ in 0..total {
+            if let Some(message) = rx.recv().await {
+                match message {
+                    Ok(delay) => {
+                        pb.set_message(format!("Addr: {}", delay.ip));
+
+                        let delay_millis = delay.consume.as_millis();
+                        if delay_millis < self.avg_delay_upper
+                            && delay_millis > self.avg_delay_lower
+                        {
+                            res.push(delay);
+                        }
+                    }
+                    Err(_) => {}
+                }
+            }
+            pb.inc(1);
+        }
+
         pb.finish_with_message("finshed");
 
         res
     }
 
-    async fn tcp_socket(&self, socket: SocketAddr) -> std::io::Result<Delay> {
+    // pub async fn run(&self) -> Vec<Delay> {
+    //     // 创建socketAddr的迭代器
+    //     let mut socket_addrs = self.ips.iter().map(|ip| SocketAddr::new(*ip, self.port));
+
+    //     let mut res: Vec<Delay> = Vec::new();
+    //     let mut ftrs = FuturesUnordered::new();
+
+    //     // 这里创建连接池的大小为 batch_size
+    //     for _ in 0..self.batch_size {
+    //         if let Some(socket) = socket_addrs.next() {
+    //             ftrs.push(self.tcp_socket(socket));
+    //         }
+    //     }
+
+    //     // 进度条
+    //     let total = self.ips.len();
+    //     let pb = ProgressBar::new(total as u64);
+    //     pb.set_style(
+    //         ProgressStyle::with_template(
+    //             "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+    //         )
+    //         .unwrap()
+    //         .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ "),
+    //     );
+
+    //     /// 只有连接池有空余才塞入任务
+    //     while let Some(result) = ftrs.next().await {
+    //         if let Some(socket) = socket_addrs.next() {
+    //             ftrs.push(self.tcp_socket(socket));
+    //         }
+
+    //         match result {
+    //             Ok(delay) => {
+    //                 pb.set_message(format!("Addr: {}", delay.ip));
+    //                 pb.inc(1);
+
+    //                 let delay_millis = delay.consume.as_millis();
+    //                 if delay_millis < self.avg_delay_upper && delay_millis > self.avg_delay_lower {
+    //                     res.push(delay);
+    //                 }
+    //             }
+    //             Err(_) => {
+    //                 pb.inc(1);
+    //             }
+    //         }
+    //     }
+
+    //     pb.finish_with_message("finshed");
+
+    //     res
+    // }
+
+    async fn tcp_socket(
+        times: NonZeroU8,
+        timeout: Duration,
+        socket: SocketAddr,
+    ) -> std::io::Result<Delay> {
         let mut total_duration = Duration::new(0, 0);
         let mut successful_calls = 0;
-        let times = self.times.get();
+        // let times = self.times.get();
 
-        for _ in 1..=times {
+        for _ in 1..=times.get() {
             let start = Instant::now();
-            let result = self.connect(socket).await;
+            let result = Scanner::connect(timeout, socket).await;
             let elapsed = start.elapsed();
 
             match result {
-                Ok(tcp_stream) => {
-                    let _ = tcp_stream.shutdown(Shutdown::Both);
+                Ok(mut tcp_stream) => {
+                    tokio::spawn(async move {
+                        tcp_stream.shutdown().await;
+                    });
 
                     successful_calls += 1;
                     total_duration += elapsed;
@@ -119,7 +196,7 @@ impl Scanner {
                     let error_string = e.to_string();
 
                     if error_string.to_lowercase().contains("too many open files") {
-                        panic!("Too many open files. Please reduce batch size.The current size is {}\nPlease try to reduce this value and then try to run again.",self.batch_size);
+                        panic!("Too many open files. Please reduce batch size\nPlease try to reduce this value and then try to run again.");
                     }
                 }
             }
@@ -132,12 +209,14 @@ impl Scanner {
         })
     }
 
-    async fn connect(&self, socket: SocketAddr) -> io::Result<TcpStream> {
-        let stream = io::timeout(
-            self.timeout,
-            async move { TcpStream::connect(socket).await },
-        )
-        .await?;
+    async fn connect(
+        timeout: Duration,
+        socket: SocketAddr,
+    ) -> tokio::io::Result<tokio::net::TcpStream> {
+        let stream = tokio::time::timeout(timeout, async move {
+            tokio::net::TcpStream::connect(socket).await
+        })
+        .await??;
         Ok(stream)
     }
 }
@@ -208,6 +287,8 @@ mod test {
             Duration::from_nanos(0),
             0,
             0,
+            9999,
+            0,
         );
 
         assert!(scan.batch_size > 0);
@@ -221,9 +302,9 @@ mod test {
         // Makes sure the network is available
         let addrs = vec![IpAddr::from_str("1.1.1.1").unwrap()];
 
-        let scanner = Scanner::new(addrs, 1000, Duration::from_millis(5000), 4, 443);
+        let scanner = Scanner::new(addrs, 1000, Duration::from_millis(5000), 4, 443, 9999, 0);
+
         let result = block_on(scanner.run());
-        // let result = sort_delays(result);
         assert!(!result.is_empty());
 
         // display top 10
